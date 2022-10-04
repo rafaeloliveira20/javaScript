779e1a692bc8abaa78e041135d35c6378c811542
Rafael Oliveira <110493885+rafaeloliveira20@users.noreply.github.com>
2022-10-04 15:14:33 -0300

    começando a estudar JavaScript

---
+++ javaScriptLetsCode/OperadoresAritimeticos.js
@@ -0,0 +1,9 @@
+let numero1 = 2;
+let numero2 = 3;
+
+console.log(numero1 + numero2); //SOMA
+console.log(numero1 - numero2); //SUBTRAÇÃO
+console.log(numero1*numero2); //MULTIPLICAÇÃO
+console.log(numero1/numero2); //DIVISÃO
+console.log(numero1**numero2); //EXPONENCIAÇÃO
+console.log(numero1 % numero2); // RESTO DA DIVISÃO
---
+++ javaScriptLetsCode/váriaveis.js
@@ -0,0 +1,7 @@
+
+let idade = 26 // number
+let altura = 1.79; // number
+let nome = "Rafael"; // String
+let estudando = true; // booleano (true ou false)
+
+console.log(idade)
