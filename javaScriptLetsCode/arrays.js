8bc64b0c1682b2ce4dd8ca939036e44c4690ae9e
Rafael Oliveira <110493885+rafaeloliveira20@users.noreply.github.com>
2022-10-12 15:02:08 -0300

    .

---
+++ javaScriptLetsCode/arrays.js
@@ -0,0 +1,11 @@
+let arr=["rafael",2,4,true];
+console.log(arr);
+
+console.log(arr[0]);
+console.log(arr[2]);
+console.log(arr[3]);
+console.clear();
+
+for(let i = 0; i< 4;i++){
+    console.log(arr[i]);
+}
---
+++ javaScriptLetsCode/arrays2.js
@@ -0,0 +1,9 @@
+const arr1 = [30,20,21,45,7];
+// fatiamento : slice
+console.log(arr1.slice(0,2));
+
+//adicionar elementos
+let arr2 =[];
+arr2.push(10,20,30);
+console.log(arr2);
+
